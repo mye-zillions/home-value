5bf6a59426fca0af2c42a3bdd7fb2d01e31332f2
Yvonne <>
2019-03-01 10:46:48 -0800

    Modify script to add urls

--- sdcDataGeneration/primaryDataGeneration.js
+++ sdcDataGeneration/primaryDataGeneration.js
@@ -1,6 +1,8 @@
 //To generate 10m primary records, 
 //while in sdcDataGeneration folder,
-//run file with following command
+//create empty file 'primaryentries.csv' in same folder 
+
+//run this with following command
 // to avoid heap issues:
 //node --max-old-space-size=8192 primaryDataGeneration.js 
 
@@ -15,8 +17,6 @@ var writer = csvWriter({
   sendHeaders: false
 });
 
-writer.pipe(fs.createWriteStream('./primaryentries.csv'));
-
 var numberWithCommas = (num) => {
   num = num.toString();
   var pattern = /(-?\d+)(\d{3})/;
@@ -25,28 +25,6 @@ var numberWithCommas = (num) => {
   }
   return num;
 };
-
-for (let i = 0; i < 10001; i++) {
-
-  let zestimationPricea = numberWithCommas(faker.random.number({'min': 500000, 'max': 5000000}));
-  let thirtyDayPriceChangea = numberWithCommas(faker.random.number({'min': 15000, 'max': 50000}));
-  let oneYearForcasta = numberWithCommas(faker.random.number({'min': 500000, 'max': (500000 + 100000)}));
-  let comparableHomePricea = numberWithCommas(faker.random.number({'min': (500000 - 100000), 'max': (500000 + 100000)}));
-  let marketAppreciationPricea = numberWithCommas(faker.random.number({'min': (500000 - 200000), 'max': (500000)}));
-  
-
-  writer.write(
-    {
-      zestimationPrice: zestimationPricea,
-      thirtyDayPriceChange: thirtyDayPriceChangea,
-      oneYearForcast: oneYearForcasta,
-      comparableHomePrice: comparableHomePricea,
-      marketAppreciationPrice: marketAppreciationPricea
-    }
-  );
-}
-
-writer.end();
 
 for (let j = 0; j < 1000; j++) {
   writer = csvWriter({
@@ -58,25 +36,41 @@ for (let j = 0; j < 1000; j++) {
 
   writer.pipe(fs.createWriteStream('./primaryentries.csv', {flags: 'a'}));
 
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 1; i <= 10000; i++) {
 
+    let id = i + (j * 10000);
     let zestimationPricea = numberWithCommas(faker.random.number({'min': 500000, 'max': 5000000}));
     let thirtyDayPriceChangea = numberWithCommas(faker.random.number({'min': 15000, 'max': 50000}));
     let oneYearForcasta = numberWithCommas(faker.random.number({'min': 500000, 'max': (500000 + 100000)}));
     let comparableHomePricea = numberWithCommas(faker.random.number({'min': (500000 - 100000), 'max': (500000 + 100000)}));
     let marketAppreciationPricea = numberWithCommas(faker.random.number({'min': (500000 - 200000), 'max': (500000)}));
-
+    let url = `https://s3-us-west-1.amazonaws.com/homevalueimg/${i % 1000}.jpg`;
+    let sellDate = faker.date.between( '2018-12-01', '2019-02-28');
+    let sellPrice = numberWithCommas(faker.random.number({'min': 500000, 'max': 5000000}));
+    let beds = faker.random.number({'min': 2, 'max': 6});
+    let baths = faker.random.number({'min': 2, 'max': 4});
+    let streetAddress = faker.address.streetAddress();
+    let priceSqft = numberWithCommas(faker.random.number({'min': 1200, 'max': 2500}));
+    let saleToList = faker.random.number({'min': 91, 'max': 105});
+    
     writer.write(
       {
+        id: id,
         zestimationPrice: zestimationPricea,
         thirtyDayPriceChange: thirtyDayPriceChangea,
         oneYearForcast: oneYearForcasta,
         comparableHomePrice: comparableHomePricea,
-        marketAppreciationPrice: marketAppreciationPricea
+        marketAppreciationPrice: marketAppreciationPricea,
+        url: url,
+        sellDate: sellDate,
+        sellPrice: sellPrice,
+        beds: beds,
+        baths: baths,
+        streetAddress: streetAddress,
+        priceSqft: priceSqft,
+        saleToList: saleToList
       }
     );
-    
   }
-
   writer.end();
 }
