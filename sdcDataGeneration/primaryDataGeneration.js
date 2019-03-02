b579a0942ce5d46c349918006ba1051cdda42832
Yvonne <>
2019-03-01 22:41:43 -0800

    Refactor seed script to format date object

--- sdcDataGeneration/primaryDataGeneration.js
+++ sdcDataGeneration/primaryDataGeneration.js
@@ -8,10 +8,13 @@
 // to avoid heap issues:
 //node --max-old-space-size=8192 primaryDataGeneration.js 
 
+//to check file length afterwards use the following command in terminal:
+//wc -l <filename>
 
 const fs = require('fs');
 const faker = require('faker');
 var csvWriter = require('csv-write-stream');
+var moment = require('moment');
 
 var writer = csvWriter({
   separator: ';',
@@ -39,8 +42,8 @@ for (let j = 0; j < 10000; j++) {
     sendHeaders: false
   });
 
-  //place target filename in fs.createWriteStream
-  writer.pipe(fs.createWriteStream('./sdcDataGeneration/primaryentries17.csv', {flags: 'a'}));
+  //place target filename in fs.createWriteStream. If file does not exist, it will create one 
+  writer.pipe(fs.createWriteStream('./sdcDataGeneration/primaryentries18.csv', {flags: 'a'}));
 
   for (let i = 1; i <= 1000; i++) {
 
@@ -52,11 +55,10 @@ for (let j = 0; j < 10000; j++) {
     let comparableHomePricea = faker.random.number({'min': (500000 - 100000), 'max': (500000 + 100000)});
     let marketAppreciationPricea = faker.random.number({'min': (500000 - 200000), 'max': (500000)});
     let url = `https://s3-us-west-1.amazonaws.com/homevalueimg/${photoid.toString().padStart(5, '0')}.jpg`;
-    let sellDate = faker.date.between( '2018-12-01', '2019-02-28');
+    let sellDate = moment(faker.date.between( '2018-12-01', '2019-02-28')).format('M/D/YYYY');
     let sellPrice = Math.floor(Math.random() * 4500000) + 500000;
     let beds = Math.floor(Math.random() * 3) + 2;
     let baths = Math.floor(Math.random() * 3) + 1;
-    
     let streetAddress = faker.address.streetAddress();
     let priceSqft = Math.floor(Math.random() * 1300) + 1200;
     let saleToList = Math.floor(Math.random() * 14) + 91;
--- sdcDataGeneration/psPrimaryLoader.js
+++ sdcDataGeneration/psPrimaryLoader.js
@@ -1,3 +1,32 @@
 const { Client } = require('pg');
 const client = new Client();
 
+/*
+Playing with postgres commands to create tables and schema: 
+
+
+CREATE SCHEMA homeval;
+
+CREATE TABLE homeval.sample (
+  id SERIAL PRIMARY KEY, 
+  zestimationPrice INT,
+  thirtyDayPriceChange INT,
+  oneYearForcast INT,
+  comparableHomePrice INT,
+  marketAppreciationPrice INT,
+  url TEXT,
+  sellDate TIMESTAMP WITH TIME ZONE,
+  sellPrice INT,
+  beds INT,
+  baths INT,
+  streetAddress TEXT,
+  priceSqft INT,
+  saleToList INT
+);
+
+
+insertion script 
+
+COPY homeval.sample(id,zestimationPrice,thirtyDayPriceChange,oneYearForcast,comparableHomePrice,marketAppreciationPrice,url,sellDate,sellPrice,beds,baths,streetAddress,priceSqft,saleToList) 
+FROM '/Users/pony/SEC/home-value/sdcDataGeneration/testload.csv' DELIMITER ';' CSV;
+*/
