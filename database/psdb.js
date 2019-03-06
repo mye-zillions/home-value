2f10312c045973ba6f38501423ba87d9b1a336a7
Yvonne <>
2019-03-05 19:05:29 -0800

    Complete connecting client to working db

--- database/psdb.js
+++ database/psdb.js
@@ -9,24 +9,41 @@ const client = new Client({
   port: credentials.port
 });
 
+let currentid = 1;
+
 client.connect()
   .then(() => console.log('connected'))
   .catch(e => console.error('connection error', err.stack));
 
 module.exports = {
   readSingleProperty: (id, cb) => {
-    const text = 'SELECT zestimationPrice, thirtyDayPriceChange, oneYearForcast, comparableHomePrice, marketAppreciationPrice FROM homeval.homes WHERE id = $1;';
+    const text = 'SELECT zestimationPrice, thirtyDayPriceChange, oneYearForcast, comparableHomePrice, marketAppreciationPrice FROM homes2.homes WHERE id = $1;';
     const values = [id];
+
+    currentid = id;
+
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        cb(null, res.rows[0]);
+      }
+    }); 
+  },
+  readRelatedProperties: (cb) => {
+    let text = 'SELECT id, url, sellDate, sellPrice, beds, baths, streetAddress, priceSqft, saleToList FROM homes2.homes where id BETWEEN $1 AND $2;';
+    let values = [Number(currentid), Number(currentid) + 19];
+    if (currentid > 9800000) {
+      values = [Number(currentid) - 19, Number(currentid)];
+    }
 
 
     client.query(text, values, (err, res) => {
       if (err) {
         console.log(err.stack);
       } else {
-        console.log(res.rows[0]);
-        cb(null, res.rows[0]);
+        cb(null, res.rows);
       }
     }); 
   }
 };
-
--- server/model.js
+++ server/model.js
@@ -1,30 +1,48 @@
 const db = require('../database/psdb.js');
 //const db = require('../database/mongodb.js');
+
 module.exports = {
   fetchAllPropertyData: (callback) => {
-    db.readAllProperties((err, propertyData) => {
+    db.readRelatedProperties((err, propertyData) => {
       if (err) {
         callback(err);
       }
-      db.readAllComparableHomes((err, comparableHomesData) => {
-        if (err) {
-          callback(err);
-          return;
-        }
-        db.readAllLocalHomes((err, localHomesData) => {
-          if (err) {
-            callback(err);
-            return; 
-          }
-          var data = {
-            propertyData: propertyData, 
-            comparableHomesData: comparableHomesData,
-            localHomesData: localHomesData,
-          };  
-          console.log('weve fetched all of the data');
-          callback(null, data); 
+
+      let compHomesData = [];
+      let locHomesData = [];
+
+      for (let i = 0; i < 10; i++) {
+        let row = propertyData[i];
+
+        compHomesData.push({
+          url: row.url,
+          sellDate: row.selldate,
+          sellPrice: row.sellprice,
+          beds: row.beds,
+          baths: row.baths,
+          streetAddress: row.streetaddress,
+          priceSqft: row.pricesqft
         });
-      });
+      }
+      for (let i = 10; i < 20; i++) {
+        let row = propertyData[i];
+
+        locHomesData.push({
+          url: row.url,
+          sellDate: row.selldate,
+          sellPrice: row.sellprice,
+          beds: row.beds,
+          baths: row.baths,
+          streetAddress: row.streetaddress,
+          saleToList: row.saletolist
+        });
+      }
+      var data = {
+        comparableHomesData: compHomesData,
+        localHomesData: locHomesData,
+      };  
+      callback(null, data); 
+
     });
   },
   fetchSinglePropertyData: (id, callback) => {
@@ -36,11 +54,11 @@ module.exports = {
       
       //Format db data to conform to front end [int => string]
       let cleanObj = {
-        zestimationPrice: singlePropertyData.zestimationprice.toLocaleString(),
-        thirtyDayPriceChange: singlePropertyData.thirtydaypricechange.toLocaleString(),
-        oneYearForcast: singlePropertyData.oneyearforcast.toLocaleString(),
-        comparableHomePrice: singlePropertyData.comparablehomeprice.toLocaleString(),
-        marketAppreciationPrice: singlePropertyData.marketappreciationprice.toLocaleString()
+        zestimationPrice: singlePropertyData.zestimationprice,
+        thirtyDayPriceChange: singlePropertyData.thirtydaypricechange,
+        oneYearForcast: singlePropertyData.oneyearforcast,
+        comparableHomePrice: singlePropertyData.comparablehomeprice,
+        marketAppreciationPrice: singlePropertyData.marketappreciationprice
       };
      
       var singleProperty = {
@@ -81,72 +99,3 @@ module.exports = {
     } );
   }
 };
-// module.exports = {
-//   fetchAllPropertyData: (callback) => {
-//     db.readAllProperties((err, propertyData) => {
-//       if (err) {
-//         callback(err);
-//       }
-//       db.readAllComparableHomes((err, comparableHomesData) => {
-//         if (err) {
-//           callback(err);
-//           return;
-//         }
-//         db.readAllLocalHomes((err, localHomesData) => {
-//           if (err) {
-//             callback(err);
-//             return; 
-//           }
-//           var data = {
-//             propertyData: propertyData, 
-//             comparableHomesData: comparableHomesData,
-//             localHomesData: localHomesData,
-//           };  
-//           console.log('weve fetched all of the data');
-//           callback(null, data); 
-//         });
-//       });
-//     });
-//   },
-//   fetchSinglePropertyData: (id, callback) => {
-//     db.readSingleProperty(id, (err, singlePropertyData) => {
-//       if (err) {
-//         callback(err);
-//         return;
-//       }
-//       // Grab the results of the query and clean
-//       var singleProperty = {
-//         singlePropertyData: singlePropertyData
-//       };
-//       callback(null, singleProperty);
-//     });
-//   },
-//   postSinglePropertyData: (obj, callback) => {
-//     db.postSingleProperty(obj, (err, singlePropertyData) => {
-//       if (err) {
-//         callback(err);
-//         return;
-//       }
-//       callback(null, singlePropertyData);
-//     });
-//   },
-
-//   deleteSinglePropertyData: (id, callback) => {
-//     db.deleteSingleProperty(id, (err, response) => {
-//       if (err) {
-//         callback(err);
-//         return;
-//       }
-//       callback(null, response);
-//     });
-//   },
-//   updateSingleProperty: (id, data, callback) => {
-//     db.updateSingleProperty(id, data, (err, response) => {
-//       if (err) {
-//         callback(err);
-//         return;
-//       }
-//       callback(null, response);
-//     } );
-//   }
-// };
